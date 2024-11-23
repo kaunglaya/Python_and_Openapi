71f5e255471c85f027a5e99b69fb582a28653fb0
kaunglaya <kaunglaya@gmail.com>
2024-11-23 16:06:52 +0800

    123

--- lesson4/tools.py
+++ lesson4/tools.py
@@ -1,7 +1,33 @@
 PI = 3.1415926
 
 class Person:
-    pass
+    '''
+    我是person
+    '''
+    def __init__(self,name:str,age:int):
+        self.name = name #attribute 儲存值
+        self.age = age #attribute
+#實體方法(instance method)
+    def echo(self):
+        print(f'我的名字是:{self.name}')
+        print(f'我的歲數是:{self.age}')
+
+class Student(Person):
+    '''
+    我是學生
+    '''
+    def __init__(self,name:str,age:int,score:int):
+        super().__init__(name=name,age=age) #將上面的def init(self)的名字跟歲數繼承
+        #super() 可以將原有多個儲存值寫在一起後繼承 -> 用一個程式碼代表
+        self.__score = score
+
+    @property #與屬性一樣故不用() 只能傳出去 無法傳入 所以不能修改
+    def score(self)->int:
+        return self.__score #分數會被保護 無法被修改
+
+    def echo(self):
+        super().echo() #將上面的def echo(self)的名字跟歲數繼承下來
+        print(f'我的分數是:{self.score}')
 
 def get_status(bmi:float)->str:
     '''
