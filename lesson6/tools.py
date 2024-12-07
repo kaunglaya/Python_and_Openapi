fda6bd22cfacab2925fcc35297d1c4dedf1bffbd
kaunglaya <kaunglaya>
2024-12-07 14:37:49 +0800

    modify

--- lesson6/tools.py
+++ lesson6/tools.py
@@ -13,3 +13,13 @@ def get_aqi(excel_name:str) -> list[dict]:
         sheets.append(site)
 
     return sheets
+
+def get_sitenames(excel_name:str) -> list[str]:
+    data:list[dict] = get_aqi(excel_name=excel_name)
+    sitenames:list = []
+    for item in data:
+        sitenames.append(item['sitename'])
+
+    sitenames = list(set(sitenames))
+    return sitenames
+    
