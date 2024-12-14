94a23ae9a7d4f77e1c90b01ac1a9d12a297a6855
kaunglaya <kaunglaya@gmail.com>
2024-12-14 16:19:02 +0800

    modify

--- lesson7/lesson7_4.py
+++ lesson7/lesson7_4.py
@@ -4,17 +4,17 @@ st.title("次數的範例")
 if 'count' not in st.session_state: #儲存在餅乾裡，第二次的時候就不會返回到這個程式
     st.session_state['count'] = 0  
 
-increament = st.button("每次加1") #生成按鈕
+increament = st.button("功德加1") #生成按鈕
 if increament :
     st.session_state['count'] += 1
 
-increament = st.button("每次加10")
+increament = st.button("功德加10")
 if increament:
     st.session_state["count"] += 10
 
-increament = st.button("歸0")
+increament = st.button("回歸原功德")
 if increament:
     st.session_state["count"] = 0
 
-st.write("次數=",st.session_state['count'])
+st.write("功德=",st.session_state['count'])
 
